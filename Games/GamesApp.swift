dd90d269f391bd155bcfa034dad732db42a7d820
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-23 21:35:53 +0300

    project uploaded

---
+++ Games/GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesApp.swift
+//  Games
+//
+//  Created by Batuhan Berk Ertekin on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

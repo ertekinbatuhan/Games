b9263a3134f0a69b3c901be45951838c56f32a4f
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-24 20:35:24 +0300

    add Game model to project

---
+++ Games/Models/Game.swift
@@ -0,0 +1,58 @@
+//
+//  Games.swift
+//  Games
+//
+//  Created by Batuhan Berk Ertekin on 24.09.2024.
+//
+
+import Foundation
+
+// MARK: - Game
+struct Game: Codable {
+    let count: Int?
+    let next: String?
+    let previous: String?
+    let results: [GameResult]?
+    let description: String?
+}
+
+// MARK: - Result
+struct GameResult: Codable {
+    let id: Int?
+    let slug, name, released: String?
+    let tba: Bool?
+    let backgroundImage: String?
+    let rating: Double?
+    let ratingTop: Int?
+    let shortScreenshots: [ShortScreenshot]?
+    let genres: [Genre]?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, slug, name, released, tba,genres
+        case backgroundImage = "background_image"
+        case rating
+        case ratingTop = "rating_top"
+        case shortScreenshots = "short_screenshots"
+    }
+}
+
+// MARK: - Genre
+struct Genre: Codable {
+    let id: Int?
+    let name, slug: String?
+    let gamesCount: Int?
+    let imageBackground: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+    }
+}
+
+// MARK: - ShortScreenshot
+struct ShortScreenshot: Codable {
+    let id: Int?
+    let image: String?
+}
+

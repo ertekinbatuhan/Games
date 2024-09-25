420593f66e5c7d6306d5763c6ed3bd6f082bc970
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-25 10:31:54 +0300

    add APIConstant to manage base URL and API key

---
+++ Games/Network/Constant/APIConstant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Games
+//
+//  Created by Batuhan Berk Ertekin on 24.09.2024.
+//
+
+// MARK: - API Constant configuration
+struct APIConstant {
+    static let baseURL = "https://api.rawg.io/api"
+    static let apiKey = "$$$$$$$$$$$$$$$$$$$$$$$$"
+}

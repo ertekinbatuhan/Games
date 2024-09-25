437fcb2cf53011faaabcf30eb0b2385355d13b0b
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-25 13:10:05 +0300

    add GameViewModel & GameViewModelProtocol

---
+++ Games/ViewModel/GameViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  GameViewModel.swift
+//  Games
+//
+//  Created by Batuhan Berk Ertekin on 24.09.2024.
+//
+
+import Foundation
+
+// MARK: - GameViewModelProtocol
+protocol GameViewModelProtocol {
+    func loadGames(page: Int)
+}
+
+// MARK: - GameViewModel
+/// ViewModel responsible for managing game data
+final class GameViewModel: ObservableObject, GameViewModelProtocol {
+    
+    // MARK: - Published Properties
+    @Published var games: [GameResult] = []
+    @Published var isLoading = false
+    
+    // MARK: - Internal Properties
+    var currentPage: Int = 1
+    private let gameService: GameServiceProtocol
+
+    // MARK: - Initializer
+    init(gameService: GameServiceProtocol = GameService()) {
+        self.gameService = gameService
+    }
+
+    // MARK: - Load Games Function
+    /// Fetches games from the given page using the game service.
+    /// Prevents multiple requests from being made at the same time.
+    /// - Parameter page: The page number to load games from.
+    func loadGames(page: Int) {
+        // MARK: - Prevent Concurrent Loading
+        guard !isLoading else { return }
+        
+        isLoading = true
+
+        let path = NetworkPath.games(page: page)
+
+        // MARK: - Fetch Games
+        gameService.fetchGames(path: path) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let gameResults):
+                    self?.games.append(contentsOf: gameResults)
+                    self?.currentPage = page
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                self?.isLoading = false 
+            }
+        }
+    }
+}

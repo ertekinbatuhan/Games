946fa912fe57b5e835b11def87911ee9aa31b8c7
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-26 14:24:18 +0300

    add GameDetailDescriptionView to Component

---
+++ Games/Components/GameDetailDescriptionView.swift
@@ -0,0 +1,32 @@
+//
+//  DescriptionView.swift
+//  Games
+//
+//  Created by Batuhan Berk Ertekin on 26.09.2024.
+//
+
+import SwiftUI
+
+struct GameDetailDescriptionView: View {
+    let gameDetail: GameDetail
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Description")
+                    .font(.headline)
+                    .padding(.top, 5)
+                
+                Image(systemName: "info.circle")
+                    .foregroundColor(.gray)
+                    .padding(.top, 5)
+            }
+            .padding(.horizontal)
+            
+            Text(gameDetail.description?.cleanedDescription() ?? "No Description Available")
+                .font(.body)
+                .padding(.horizontal)
+                .padding(.bottom)
+        }
+    }
+}
